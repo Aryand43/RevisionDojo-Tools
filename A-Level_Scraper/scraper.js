6334a2f3d480057f984fb3b957f0253a3b4e7bd6
Aryan Dutt <aryan.dutt43@gmail.com>
2024-09-04 23:44:23 +0800

    Cleaned up the scraper.js file

--- A-Level_Scraper/scraper.js
+++ A-Level_Scraper/scraper.js
@@ -4,14 +4,13 @@ const fs = require('fs');
 
 const baseUrl = 'https://revisionworld.com/a2-level-level-revision';
 
-// Function to scrape categories and topics
 async function scrapeCategories() {
     try {
         const response = await axios.get(baseUrl);
         const $ = cheerio.load(response.data);
         const data = {};
 
-        // Scrape subjects and their links
+        //Scrape subjects and links - JPath 
         $('#taxonomy-term-5006 > div > div > div > div.block.block-revision-study-resources.block-revision-study-resources-section-child-links > div > ul > li').each((i, element) => {
             const subject = $(element).find('a').text().trim();
             const subjectUrl = $(element).find('a').attr('href');
@@ -30,7 +29,6 @@ async function scrapeCategories() {
     }
 }
 
-// Function to scrape topics and subtopics
 async function scrapeTopicsAndSubtopics(data) {
     for (const subject in data) {
         for (const topic in data[subject].Topics) {
@@ -38,8 +36,6 @@ async function scrapeTopicsAndSubtopics(data) {
             try {
                 const response = await axios.get(topicUrl);
                 const $ = cheerio.load(response.data);
-
-                // General pattern to extract subtopics
                 $('div.block-revision-study-resources-section-child-links ul li a').each((i, subtopicElement) => {
                     const subtopic = $(subtopicElement).text().trim();
                     if (subtopic) {
@@ -54,12 +50,9 @@ async function scrapeTopicsAndSubtopics(data) {
     }
 }
 
-// Main function to execute the scraping
 async function main() {
     const data = await scrapeCategories();
     await scrapeTopicsAndSubtopics(data);
-
-    // Save the data to a JSON file
     fs.writeFileSync('alevel_data.json', JSON.stringify(data, null, 2));
     console.log('Data saved to alevel_data.json');
 }
